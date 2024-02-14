6df759906ad6722386577bee3cb057a98c55952c
Aryan <pakhrinp242@gmail.com>
2024-02-14 16:19:34 +0545

    datatype

---
+++ day1/datatypes.js
@@ -0,0 +1,18 @@
+let name="aryan"
+let age=28
+let isLoggedin=false
+
+// number = 2 to power 53
+// string = " "
+// boolean = true/false
+// null = stand alone value
+// undefined = not defined of value
+// symbol = unique
+
+
+//object = 
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isLoggedin);
+console.log(typeof undefined);
+console.log(typeof null);
