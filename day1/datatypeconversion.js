fc0caf54ca6c92363a0f5a0c79568b69d477e5fe
Aryan <pakhrinp242@gmail.com>
2024-02-14 17:03:19 +0545

    conversion

---
+++ day1/datatypeconversion.js
@@ -0,0 +1,20 @@
+let score ="33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let num=Number(score)
+console.log(typeof num);
+console.log( num);
+
+// '33'>33
+// '33abc'=nan
+//true=1
+//false=0
+
+// in booloea
+// 1=>true 0=>false
+// ''=>false
+// "something"=>true
+
+
+
