b2a742ad42bb06e5ec3d418b2ff06c243d4631bc
Aryan <pakhrinp242@gmail.com>
2024-02-14 15:49:40 +0545

    s

---
+++ day1/script.js
@@ -0,0 +1,14 @@
+const accountId=1444
+let accountEmail="aryanpakhrin.com"
+var accountPassword="12345"
+let accountCity="nepal"
+let nep
+
+
+console.log("aryan");
+// accountId=12
+/*
+prefer not to use var because of scope 
+*/
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,nep])
