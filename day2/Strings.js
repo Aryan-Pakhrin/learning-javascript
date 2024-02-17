c949191d5b6a4badaec7e1261028f57d6dea1861
Aryan <pakhrinp242@gmail.com>
2024-02-17 16:16:10 +0545

    String

---
+++ day2/Strings.js
@@ -0,0 +1,21 @@
+// const name="aryan"
+// const surname="pakhrin"
+
+// console.log(`hello my name is ${name} ${surname}`);
+
+const gane= new String("aryan")
+console.log(gane.length);
+console.log(gane.toUpperCase());
+console.log(gane.toLowerCase());
+console.log(gane.charAt(0))
+console.log(gane.indexOf('r'));
+console.log(gane.substring(0,2));
+const name="        pakhrin            "
+console.log(name);
+console.log(name.trim());
+
+const hanm="hanguma"
+console.log(hanm.replace('h','w'));
+console.log(hanm.includes('r'));
+const game="hello-aryan-k"
+console.log(game.split('-'));
