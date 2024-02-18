d8c31e9f50acdf455f33dc06c08403bf4185f2bf
Aryan <pakhrinp242@gmail.com>
2024-02-18 11:23:00 +0545

    Numbers

---
+++ day3/Numbers.js
@@ -0,0 +1,24 @@
+const balance= new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(12));
+
+
+// ------------ maths--------------
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+console.log(Math.min(4,3,6));
+console.log(Math.max(9,2,3));
+console.log(Math.pow(2,4));
+console.log(Math.sqrt(25));
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+console.log(Math.ceil(Math.random()));
