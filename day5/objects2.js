fb9a0db88ab7c047b7836e9361dd0a36a2e279f1
Aryan <pakhrinp242@gmail.com>
2024-02-22 19:32:00 +0545

    two

---
+++ day5/objects2.js
@@ -0,0 +1,10 @@
+//destructuring
+const course={
+    coursename:"js",
+    price:999,
+    xourseInst:"asd"
+}
+// console.log(course.xourseInst);
+const{coursename:JADA}=course
+console.log(JADA);
+
