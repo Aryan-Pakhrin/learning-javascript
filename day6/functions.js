5f4074c26d973c6b966919f47345652d3169468b
Aryan <pakhrinp242@gmail.com>
2024-02-24 14:10:29 +0545

    func

---
+++ day6/functions.js
@@ -0,0 +1,57 @@
+// functions
+// function sayMyname(){
+//     console.log("helo");
+// }
+// sayMyname()
+
+// function addFunction( num1,num2){
+//     let result=num1+num2;
+//     return result;
+// }
+// const re =addFunction(1,3)
+// console.log(re);
+
+
+// function Loginuser(username){
+//     return `${username} just loggedin`
+// }
+// const a=Loginuser("arya")
+// console.log(a);
+
+// function Loginuser(username){
+//    if (username===undefined) {
+//     console.log("wo");
+    
+//    }
+//    else{
+//     return `${username} just loggedin`
+//    }
+// }
+// const a=Loginuser("")
+// console.log(a);
+
+// function calculate(...a){
+//     return a
+// }
+// console.log(calculate(1,2,3,4));
+
+
+// using objects in function
+
+
+// const user={
+//     username:"arya",
+//     location:"pakh"
+// }
+// function hand(anyobj){
+//     console.log(`username is ${anyobj.username} and  price is ${anyobj.location}`);
+// }
+// hand(user)
+
+// using array in function
+
+const mynewarr=[1,2,3,4,5]
+function getarr(kdk){
+    return mynewarr[1]
+}
+console.log(getarr(mynewarr));
---
+++ day6/scope.js
@@ -0,0 +1,31 @@
+// let a=12
+// const b=2
+// if (true) {
+//     let a=90
+//     console.log(a);
+    
+// }
+// console.log(a);
+
+// function one(){
+//     const username="hitesh"
+
+//     function two(){
+//             const website="youtube"
+//             console.log(username);
+
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+console.log(Name("arya"));
+function Name(name){
+    return name;
+}
+
+const npc=function(bull){
+    return bull
+}
+npc("shit")
