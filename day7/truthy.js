58b53daa0a6fa56d8940451fd75f9942085af587
Aryan <pakhrinp242@gmail.com>
2024-02-27 18:45:04 +0545

    day7

---
+++ day7/truthy.js
@@ -0,0 +1,30 @@
+// truthy or falsy value
+
+const userEmail="hitesh.ai"
+if (userEmail) {
+    console.log("got user_email");
+}else{
+    console.log("don't have user e_mail");
+}
+
+// falsy value
+
+// false,0,-0,BigInt,"",null. undefined,NaN
+
+// truthy value
+// "" if space is given in empty string then it is assumed as true
+// "0",[],'false',' ',{},function()
+
+//  to check Array
+// const arr=[]
+// if (arr.length===0) {
+//     console.log("it is array");  
+// }
+
+// to check Object
+
+// const Obj={}
+// if (Object.keys(Obj).length===0) {
+//     console.log("empty");   
+// }
+
