a64e6e50c96dac18a7ab1a23ec4912a89bf253bb
Aryan <pakhrinp242@gmail.com>
2024-03-05 15:37:36 +0545

    dom

---
+++ day11/script.js
@@ -0,0 +1,16 @@
+const parent=document.querySelector('.parent')
+// console.log(parent.children);
+// console.log(parent.children[1].innerHTML);
+
+// for(let i=0;i<parent.children.length;i++){
+//     console.log(parent.children[i].innerHTML);
+// }
+// const a=console.log(parent.firstElementChild);
+// console.log(a);
+// console.log(a.nextElementSibling);
+// console.log(parent.lastElementChild);
+// const parent=document.querySelector('.day')
+// console.log(parent);
+// console.log(parent.nextElementSibling);
+console.log(parent.childNodes);
+parent.children[1].style.color="red"
