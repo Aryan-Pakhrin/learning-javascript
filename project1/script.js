15c3f07e5094575d60a41b7e7daee6f2fb49a3e6
Aryan <pakhrinp242@gmail.com>
2024-03-07 19:27:09 +0545

    random

--- project1/script.js
+++ project1/script.js
@@ -5,3 +5,12 @@ const emojis=["🤣","😁","😒","😢"]
 emoji.addEventListener("mouseover",()=>{
     emoji.innerHTML=emojis[Math.floor(Math.random()*emojis.length)]
 })
+
+
+const click=document.querySelector(".click")
+
+const text=["A","b","c","d","clickme"]
+
+click.addEventListener('click',()=>{
+    click.innerHTML=text[Math.floor(Math.random()*text.length)]
+})
